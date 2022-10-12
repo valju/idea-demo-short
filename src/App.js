5dfed0aff387c88a6ada7f9ae90c27cb57a1c73e
valju <juhani.valimaki@haaga-helia.fi>
2022-10-12 17:09:36 +0300

    Home view and routing and navigation to it added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import HomeView from './views/HomeView';
 import CategoryListView from './views/CategoryListView';
 import CategoryDetailsView from './views/CategoryDetailsView';
 
@@ -8,9 +9,12 @@ function App() {
       <div className="App">
         <nav>
           <span>Extra navbar at top: </span>
+          <NavLink to="/home">Home</NavLink>
+          <span> </span>
           <NavLink to="/category">Categories</NavLink>
         </nav>
         <Routes>
+          <Route path="home" element={<HomeView />} />
           <Route path="category" element={<CategoryListView />} />
           <Route path="/category/:categoryId" element={<CategoryDetailsView />} />
         </Routes>
---
+++ src/views/HomeView.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HomeView = () => {
+    return(
+        <div>
+            <h1>Welcome to the Idea case!</h1>
+            {/*
+            <dl>
+                <dt>Date:</dt>
+                <dd>{Date.now.toString("yyy-MM-dd")}</dd>
+                <dt>Time:</dt>
+                <dd>{Date.now.toString("HH:mm")}</dd>
+            </dl>
+            */}          
+        </div>
+
+    );
+}
+
+export default HomeView;
